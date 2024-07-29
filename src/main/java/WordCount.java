082dbeacbfdd6ba5d6e672a9e3700db2ee7a16ab
masumcse1 <optinfra.masum@gmail.com>
2024-07-29 19:43:13 +0600

    streaming data

---
+++ src/main/java/WordCount.java
@@ -0,0 +1,38 @@
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class WordCount {
+
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        final ParameterTool params = ParameterTool.fromArgs(args);
+        env.getConfig().setGlobalJobParameters(params);
+        DataStream<String> text = env.socketTextStream("localhost", 9999);
+
+        DataStream<Tuple2<String, Integer>> counts = text.filter(new FilterFunction<String>() {
+                    public boolean filter(String value) {
+                        return value.startsWith("N");
+                    }
+                })
+
+                .map(new Tokenizer())         // split up the lines in pairs (2-tuples) containing: tuple2 {(name,1)...}
+                .keyBy(0).sum(1);            // group by the tuple field "0" and sum up tuple field "1"
+
+        counts.print();
+
+        // execute program
+        env.execute(" WordCount example datastreaming");
+    }
+
+    public static final class Tokenizer implements MapFunction<String, Tuple2<String, Integer>> {
+        public Tuple2<String, Integer> map(String value) {
+            return new Tuple2(value, Integer.valueOf(1));
+        }
+    }
+
+}
